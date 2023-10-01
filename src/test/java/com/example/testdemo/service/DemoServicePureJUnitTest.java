650658bd9f6cf2dd7f3d46deb5ccc2f05f118a2b
Jakub Nietupski <jnie@netcompany.com>
2023-10-01 20:21:35 +0200

    add junit test

--- src/test/java/com/example/testdemo/service/DemoServicePureJUnitTest.java
+++ src/test/java/com/example/testdemo/service/DemoServicePureJUnitTest.java
@@ -1,22 +1,21 @@
 package com.example.testdemo.service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DemoServiceTest {
+class DemoServicePureJUnitTest {
 
     @Test
     void whenIncomeIsFirstCategoryThenTaxShouldBeFirstLevel() {
         final var demoService = new DemoService();
         final var income = new BigDecimal("50");
-        final var expectedTax = new BigDecimal("5");
+        final var expectedTax = new BigDecimal("5.0");
 
         final var actualTax = demoService.calculateTax(income);
 
-        assertTrue(expectedTax.equals(actualTax));
+        assertEquals(expectedTax, actualTax);
     }
 }
