e22db8e6fd25db2a3dfa49e5e00e05c39cf28347
Jakub Nietupski <jnie@netcompany.com>
2023-10-01 20:24:38 +0200

    add spring boot test

--- src/test/java/com/example/testdemo/service/DemoServiceSpringBootTest.java
+++ src/test/java/com/example/testdemo/service/DemoServiceSpringBootTest.java
@@ -1,19 +1,22 @@
 package com.example.testdemo.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// Here the Spring context will not be started,
-// thus the tests will run faster
-// but this is a very simple example which assumes that there are no dependencies on DemoService
-class DemoServicePureJUnitTest {
+// Here the whole Spring context will be started
+@SpringBootTest
+public class DemoServiceSpringBootTest {
+
+    @Autowired
+    private DemoService demoService;
 
     @Test
     void whenIncomeIsFirstCategoryThenTaxShouldBeFirstLevel() {
-        final var demoService = new DemoService();
         final var income = new BigDecimal("50");
         final var expectedTax = new BigDecimal("5.0");
 
